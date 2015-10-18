e1ce44a1b9ab2c9f6cded1563ba0ec8505542c67
進撃の技術者 <refusebt@sina.com>
2015-10-19 00:12:46 +0800

    create

---
+++ RFDestoryNotify/RFDestoryNotify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFDestoryNotify
+//
+//  Created by gouzhehua on 15/10/18.
+//  Copyright © 2015年 gouzhehua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
